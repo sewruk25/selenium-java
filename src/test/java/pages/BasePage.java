9fccf7c78b753261217a50b9ec4301f69a6452a6
Kacper Sewruk <kacpersewruk@gmail.com>
2023-07-18 15:14:02 +0200

    trial

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -2,11 +2,14 @@ package pages;
 
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
+
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -15,6 +18,7 @@ public class BasePage {
 
     protected WebDriverWait wait;
     protected WebDriver driver;
+
 
 
     public BasePage(WebDriver driver) {
